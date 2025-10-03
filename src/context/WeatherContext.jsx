11967583ef6a5ae825c12112994c91c645f80138
Vikas Solanki <solankivikas066@gmail.com>
2025-10-03 23:36:09 +0530

    feat: load initial weather data in WeatherProvider

--- src/context/WeatherContext.jsx
+++ src/context/WeatherContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useWeatherService } from "../hooks/useWeatherService";
 
 const WeatherContext = createContext(null)
 
@@ -11,8 +12,17 @@ export const useWeather = () => {
 }
 
 export const WeatherProvider = ({children}) => {
+  const {fetchWeatherData} = useWeatherService()
   const [weatherData, setWeatherData] = useState(null)
-  const [selectedGeoLoc, setSelectedGeoLoc] = useState({latitude: '', longitude: ''})
+  const [selectedGeoLoc, setSelectedGeoLoc] = useState({latitude: '19.07283', longitude: '72.88261', name: 'Mumbai, India'})
+
+  useEffect(() => {
+    const loadWeather = async () => {
+      const data = await fetchWeatherData(selectedGeoLoc.latitude, selectedGeoLoc.longitude)
+      setWeatherData(data)
+    }
+    loadWeather()
+  }, [])
 
   const value = {
     weatherData, setWeatherData,
