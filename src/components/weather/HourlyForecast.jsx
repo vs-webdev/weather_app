8604e144147eb477e850e932c1f6218d8bc2dd37
Vikas Solanki <solankivikas066@gmail.com>
2025-10-22 16:41:33 +0530

    feat: implement day-based hourly forecast selection with 3-hou interval data

--- src/components/weather/HourlyForecast.jsx
+++ src/components/weather/HourlyForecast.jsx
@@ -1,31 +1,59 @@
+import { useMemo, useState } from "react"
 import weatherIcon from "../../assets/images/icon-drizzle.webp"
 import { useWeather } from "../../context/WeatherContext"
 
 const HourlyForecast = () => {
   const {weatherData} = useWeather()
+  const [selectedDay, setSelectedDay] = useState(0)
+  
+  const {time, temperature_2m, weather_code} = weatherData?.hourly || {}
+
+  const getHourlyForecastData = (dayIndex) => {
+    const start = dayIndex * 24
+    const end = start + 24
+    const result = time?.slice(start, end)
+    .filter((_, i) => i % 3 === 0)
+    .map((t, i) => ({
+      time: new Date(t).toLocaleTimeString("en-US", {hour: "numeric", minute: "2-digit"}),
+      temperature: temperature_2m[start + i * 3],
+      weatherCode: weather_code[start + i * 3]
+    }))
+    return result
+  }
+
+  const hourlyForecastData = useMemo(
+    () => getHourlyForecastData(selectedDay),
+    [weatherData, selectedDay]
+  )
+  
+  const weekdays = weatherData?.daily?.time?.map(d => {
+    return new Date(d).toLocaleString("en-US", {weekday: "long"})
+  })
+  
   if (!weatherData){
     return <h1>loading</h1>
   }
-  
-  const {time, temperature_2m, weather_code} = weatherData?.hourly
-
-  const hourlyForecastData = time?.slice(0, 24).filter((_, i) => i % 3 === 0).map((t, i) => {
-    const formattedTime = new Date(t).toLocaleTimeString("en-US", {hour: "numeric", minute: "2-digit"})
-    return {
-      time: formattedTime,
-      temperature: temperature_2m[i * 3],
-      weatherCode: weather_code[i * 3]
-    }
-  })
 
   return (
-    <div className="px-3 py-5 bg-gray-600/10 h-full">
-      <ul className="flex mb-3">
+    <div className="flex flex-col items-end px-6 py-5 bg-gray-600/10 h-full">
+      <div className="flex gap-4 mb-2">
+        <span>Hourly Forecast</span>
+          <select name="day" id="day" onChange={(e) => setSelectedDay(Number(e.target.value))}>
+            {weekdays?.map((day, index) => (
+              <option key={index} value={index} className="border bg-neutral-800">
+                {day}
+              </option>
+            ))}
+          </select>
+      </div>
+      <ul className="flex gap-6">
         {hourlyForecastData.map((data, index) => (
-        <li key={index} className="mr-5 flex flex-col items-center">
-          <img src={weatherIcon} alt="Weather Icon" className="w-15" />
-          <span className="text-2xl">{data.temperature}&deg;</span>
-          <p className="text-sm mt-2">{data.time}</p>
+        <li key={index} className="flex flex-col items-center">
+          <div>
+            <img src={weatherIcon} alt="Weather Icon" className="w-15" />
+            <span className="text-2xl">{data.temperature}&deg;</span>
+            <p className="text-sm mt-2">{data.time}</p>
+          </div>
         </li>
         ))}
       </ul>
